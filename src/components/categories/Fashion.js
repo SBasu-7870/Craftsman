67909a289bf6ba392a7eab0ac0052f1d3bc54e20
SBasu-7870 <rick05basu@gmail.com>
2021-11-23 19:09:24 +0530

    commit 2

---
+++ src/components/categories/Fashion.js
@@ -0,0 +1,114 @@
+import React from "react";
+
+const Fashion = () => {
+  return (
+    <>
+      <div className="container">
+        <h1 className="mx-2">Fashion</h1>
+        <div className="container my-5">
+          <h2>Mens</h2>
+          <div className="row">
+            <div className="col-md-4">
+              <div className="card">
+                <img src="..." className="card-img-top" alt="..." />
+                <div className="card-body">
+                  <h5 className="card-title">Card title</h5>
+                  <p className="card-text">
+                    Lorem ipsum dolor sit amet consectetur adipisicing elit.
+                    Earum eveniet nisi itaque labore facere a illum vel officia
+                    asperiores culpa, odio veniam voluptatibus molestiae
+                    aspernatur unde iure eius aliquid sequi recusandae quisquam!
+                  </p>
+                </div>
+              </div>
+            </div>
+
+            <div className="col-md-4">
+              <div className="card">
+                <img src="..." className="card-img-top" alt="..." />
+                <div className="card-body">
+                  <h5 className="card-title">Card title</h5>
+                  <p className="card-text">
+                    Lorem ipsum dolor sit amet consectetur adipisicing elit.
+                    Impedit maiores iusto minima, iste repellat et consequatur!
+                    Deserunt, explicabo. Laborum animi reprehenderit cum,
+                    dolorem suscipit vel error a molestias, autem explicabo
+                    tempore molestiae!
+                  </p>
+                </div>
+              </div>
+            </div>
+
+            <div className="col-md-4">
+              <div className="card">
+                <img src="..." className="card-img-top" alt="..." />
+                <div className="card-body">
+                  <h5 className="card-title">Card title</h5>
+                  <p className="card-text">
+                    Lorem ipsum dolor sit, amet consectetur adipisicing elit.
+                    Error, illum deserunt. Deleniti omnis aliquam nobis
+                    laboriosam voluptatibus id, veniam quod. Dolore ratione odit
+                    illo dolorum autem, nisi asperiores ea alias sunt sint.
+                  </p>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+
+        <div className="container my-5">
+          <h2>Womens</h2>
+          <div className="row">
+            <div className="col-md-4">
+              <div className="card">
+                <img src="..." className="card-img-top" alt="..." />
+                <div className="card-body">
+                  <h5 className="card-title">Card title</h5>
+                  <p className="card-text">
+                    Lorem ipsum dolor sit amet consectetur adipisicing elit.
+                    Earum eveniet nisi itaque labore facere a illum vel officia
+                    asperiores culpa, odio veniam voluptatibus molestiae
+                    aspernatur unde iure eius aliquid sequi recusandae quisquam!
+                  </p>
+                </div>
+              </div>
+            </div>
+
+            <div className="col-md-4">
+              <div className="card">
+                <img src="..." className="card-img-top" alt="..." />
+                <div className="card-body">
+                  <h5 className="card-title">Card title</h5>
+                  <p className="card-text">
+                    Lorem ipsum dolor sit amet consectetur adipisicing elit.
+                    Impedit maiores iusto minima, iste repellat et consequatur!
+                    Deserunt, explicabo. Laborum animi reprehenderit cum,
+                    dolorem suscipit vel error a molestias, autem explicabo
+                    tempore molestiae!
+                  </p>
+                </div>
+              </div>
+            </div>
+
+            <div className="col-md-4">
+              <div className="card">
+                <img src="..." className="card-img-top" alt="..." />
+                <div className="card-body">
+                  <h5 className="card-title">Card title</h5>
+                  <p className="card-text">
+                    Lorem ipsum dolor sit, amet consectetur adipisicing elit.
+                    Error, illum deserunt. Deleniti omnis aliquam nobis
+                    laboriosam voluptatibus id, veniam quod. Dolore ratione odit
+                    illo dolorum autem, nisi asperiores ea alias sunt sint.
+                  </p>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </>
+  );
+};
+
+export default Fashion;
